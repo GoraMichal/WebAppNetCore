8f77e0ba0eb7ae625560caace844bd397de4749b
Michał Góra <michalgorazs3@gmail.com>
2020-04-15 16:01:57 +0200

    Sort, Search (linq.expression)

--- WebAppNetCore/Models/Pages/QueryOptions.cs
+++ WebAppNetCore/Models/Pages/QueryOptions.cs
@@ -9,5 +9,11 @@ namespace WebAppNetCore.Models.Pages
     {
         public int CurrentPage { get; set; } = 1;
         public int PageSize { get; set; } = 10;
+
+        public string OrderPropertyName { get; set; }
+        public bool DescendingOrder { get; set; }
+
+        public string SearchPropertyName { get; set; }
+        public string SearchTerm { get; set; }
     }
 }

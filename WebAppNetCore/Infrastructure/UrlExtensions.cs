24044c81bfcc355ed5c54e4a878ff8b50b8022b3
Michał Góra <michalgorazs3@gmail.com>
2020-05-04 20:50:12 +0200

    ViewStore + new URL extension

---
+++ WebAppNetCore/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace WebAppNetCore.Infrastructure
+{
+    public static class UrlExtensions
+    {
+        //nowy URL + nalezy dodac namespace do _ViewImports.cshtml
+        public static string PathAndQuery(this HttpRequest request) =>
+             request.QueryString.HasValue
+                 ? $"{request.Path}{request.QueryString}"
+                 : request.Path.ToString();
+    }
+}
